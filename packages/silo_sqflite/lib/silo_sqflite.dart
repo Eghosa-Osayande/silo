2e5bcc5fcdefb19bf0c76768d95f346eba74400a
Eghosa-Osayande <osayandeeghosam@gmail.com>
2025-05-19 22:33:13 +0100

    export sqflite and sqlite3 from drivers

--- packages/silo_sqflite/lib/silo_sqflite.dart
+++ packages/silo_sqflite/lib/silo_sqflite.dart
@@ -1,4 +1,5 @@
 library;
 
 export 'package:silo/silo.dart';
+export 'package:sqflite/sqflite.dart';
 export 'src/driver/database.dart' show DBSqflite;
--- packages/silo_sqlite3/lib/silo_sqlite3.dart
+++ packages/silo_sqlite3/lib/silo_sqlite3.dart
@@ -1,5 +1,6 @@
 library;
 
 export 'package:silo/silo.dart';
+export 'package:sqlite3/sqlite3.dart';
 export 'src/driver/database.dart' show DBSqlite3;
 
