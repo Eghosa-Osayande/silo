600a17dd9609c275022b8cb57c8ca69960f8b03d
Eghosa-Osayande <osayandeeghosam@gmail.com>
2025-05-24 11:57:56 +0100

    refactor migrator interface

--- packages/silo/lib/src/drivers/interfaces/migrator.dart
+++ packages/silo/lib/src/drivers/interfaces/migrator.dart
@@ -1,11 +1,13 @@
 import 'package:silo/src/silo/models.dart';
 
 abstract class Migrator {
-  Future<void> createJsonTable(String name);
+  Future<void> createValueTable(String name);
 
   Future<void> autoMigrateSiloTable<T>(SiloTable<T> table);
 
   Future<bool> hasTable(String name);
 
   String typeToTableName(Type t);
+
+  Future<Set<String>> getColumnNames(String tableName);
 }
